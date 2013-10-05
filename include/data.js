61947a1ad282334c102a482d9be4812a3c231378
eugene wu <sirrice@gmail.com>
2013-10-05 02:08:30 -0400

    added gallery

--- include/data.js
+++ include/data.js
@@ -1,3 +1,40 @@
+
+var render = function (templateid, targetid, data) {
+  var source   = $(templateid).html();
+  var template = Handlebars.compile(source);
+  var html    = template(data);
+  $(targetid).html(html);
+}
+
+var renderGallery = function (templateid, targetid, data) {
+  var source   = $(templateid).html();
+  var template = Handlebars.compile(source);
+  var html    = template(data);
+
+  $(targetid).html(html);
+  $(targetid).find(".image").each(function() {
+    var el = $(this);
+    var url = el.css("background-image");
+    url = url.substr(4, url.length-5);
+
+    var img = $("<img class='hidden'></img>")
+      .attr("src", url)
+      .load(function() {
+        var w = img.width();
+        var h = img.height();
+        var ratio = 100.0 / h;
+        h = 100;
+        w = ratio * w;
+        el.width(w).height(h);
+        console.log([img.width(), img.height()]);
+      });
+    $("body").append(img);
+  });
+}
+
+
+
+
 var papers = { 
   papers: [
    { 
@@ -157,3 +194,41 @@ var papers = {
   ]
 };
 
+
+
+var shirts = [
+  "magneto.png",
+  "shredder.png",
+  "skeletor.png",
+  "bitsfactory.png",
+  "robotassemble.png",  
+  "2pacbiggie.png",
+  "ahab.png",
+  "bearninja.png",
+  "dinospace.png",
+  "penguin.png"
+];
+
+var misc = [  
+  "cameraculture.png",
+  "caveman.png",
+  "turtle.png",
+
+];
+
+var posters = [
+  "assassin.png",
+  "sithhappens.png",
+  "superheros.png"
+]
+
+var ultimate = [
+  "grimbeaver.png",
+  "grim2.png",
+  "ghostface.png",
+  "rambo.png",  
+  "mixednuts.png",
+  "mixedfront.png",
+
+
+]
