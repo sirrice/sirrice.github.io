ef997096a1502f16bef6b467def3b3cef89edff3
ewu <ewu@cs.columbia.edu>
2019-05-08 23:47:15 -0400

    updated site

--- files/js/mypic.js
+++ files/js/mypic.js
@@ -1,7 +1,12 @@
-
-if (Math.random() > 0.5) {
+if (!/pic=true/.test(document.cookie)) {
+  document.cookie = "pic=true;max-age=315360000";
+} else {
+  document.cookie = "pic=false;max-age=315360000";
   var css = document.createElement('style');
   css.type = 'text/css';
   css.appendChild(document.createTextNode("#mypic:hover { background-image:url(/files/images/eugene.png); }"))
   document.querySelector("body").appendChild(css);
+}
+
+if (Math.random() > 0.5) {
 }
